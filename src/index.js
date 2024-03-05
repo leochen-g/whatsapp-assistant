ca5ff0b244863853b36b8b6bf9d9e8d7892dad32
leo_chen <leo.gengchen@foxmail.com>
2024-03-05 15:16:12 +0800

    fix: 添加详细日志

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import {WechatyBuilder} from '@juzi/wechaty'
+import {WechatyBuilder, log } from '@juzi/wechaty'
 import {WechatyWebPanelPlugin} from 'wechaty-web-panel'
 import {PuppetWhatsapp} from '@juzi/wechaty-puppet-whatsapp'
 
 const name = 'whatsapp-assistant';
 let bot = '';
-
+log.level('verbose')
 const puppet = new PuppetWhatsapp()
 bot = WechatyBuilder.build({
     name, // generate xxxx.memory-card.json and save login data for the next login
@@ -13,7 +13,7 @@ bot = WechatyBuilder.build({
 
 
 bot.use(WechatyWebPanelPlugin({
-    apiKey: '微秘书平台 apiKey', apiSecret: '微秘书平台 apiSecret',
+    apiKey: '7cb82c5b0090977ee013e881ace9b5f2b827dfee', apiSecret: 'b575b3a21ff47464570c6d4dc93868ec8ad548b1',
 }))
 bot.start()
     .catch((e) => console.error(e));
